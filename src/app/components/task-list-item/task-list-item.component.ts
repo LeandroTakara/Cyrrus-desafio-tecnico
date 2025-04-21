9c2d24e209dfae5e24b3dc64d19729512914cc2e
Leandro Kenzo Takara <leandroktkr@gmail.com>
2025-04-21 14:33:55 -0300

    style task

--- src/app/components/task-list-item/task-list-item.component.ts
+++ src/app/components/task-list-item/task-list-item.component.ts
@@ -1,12 +1,29 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Task } from '../../types/task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark, faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-list-item',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './task-list-item.component.html',
   styleUrl: './task-list-item.component.css'
 })
 export class TaskListItemComponent {
   task = input.required<Task>();
+
+  // icons
+  faXmark = faXmark;
+  faCheck = faCheck;
+  faTrash = faTrash;
+
+  priority = computed(() => {
+    if (this.task().priority === 'low') return 'baixa';
+    if (this.task().priority === 'medium') return 'média';
+    return 'alta';
+  })
+
+  toggleCompleted() {
+    this.task().completed = !this.task().completed;
+  }
 }
